3f1d0bb0520c129e7e1a2804cf3516531092bbb4
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2025-01-03 17:17:44 +0530

    Update server.py

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ app.add_middleware(
 nlp = spacy.load("de_core_news_sm")
 
 @app.post("/tokenize-sentences")
-async def tokenize_sentences(text: str = Body(...)):
+async def tokenize_sentences(text: str = Form(...)):
     doc = nlp(text)
     sentences = [sent.text for sent in doc.sents]
     return {"sentences": sentences}
